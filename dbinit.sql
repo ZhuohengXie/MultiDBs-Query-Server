d425374a86f5ec8ac36fa0c6507161fea0109974
ndsorrowchi <ndsorrowchi@gmail.com>
2015-03-23 19:40:19 -0400

    presto embedded

---
+++ dbinit.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE  IF NOT EXISTS 'infsci2711_tutorial' /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE 'infsci2711_tutorial';
+-- MySQL dump 10.13  Distrib 5.6.11, for osx10.6 (i386)
+--
+-- Host: localhost    Database: infsci2711_tutorial
+-- ------------------------------------------------------
+-- Server version	5.6.13-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table 'Person'
+--
+
+DROP TABLE IF EXISTS 'Person';
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE 'Person' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'firstName' varchar(45) DEFAULT NULL,
+  'lastName' varchar(45) DEFAULT NULL,
+  PRIMARY KEY ('id')
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS 'test1';
+DROP TABLE IF EXISTS 'test2';
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE 'test1' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'value' int(11) DEFAULT NULL,
+  PRIMARY KEY ('id')
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+CREATE TABLE 'test2' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'number' int(11) DEFAULT NULL,
+  PRIMARY KEY ('id')
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO 'test1'
+VALUES (1,2);
+INSERT INTO 'test1'
+VALUES (3,4);
+INSERT INTO 'test1'
+VALUES (5,6);
+INSERT INTO 'test1'
+VALUES (7,8);
+
+INSERT INTO 'test2'
+VALUES (2,123);
+INSERT INTO 'test2'
+VALUES (3,456);
+INSERT INTO 'test2'
+VALUES (5,789);
+INSERT INTO 'test2'
+VALUES (8,147);
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-03-17  9:55:15
+
+CREATE USER 'group2'@'localhost' IDENTIFIED BY 'infsci2711';
+grant all privileges on *.* to 'dataverse'@'localhost';
+
+CREATE USER 'multidbsuser'@'%' IDENTIFIED BY 'infsci2711';
+grant all privileges on *.* to 'dataverse'@'%';
+
+FLUSH PRIVILEGES;
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#install oracle java 8
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+#install others
+sudo apt-get install maven
+sudo apt-get install mysql-server
+sudo apt-get install nginx
+
+#folders and environments
+cd /opt
+sudo mkdir project
+cd project
+
+#clone project
+sudo git clone https://github.com/infsci2711/MultiDBs-Query-Server.git
+sudo git clone https://github.com/infsci2711/MultiDBs-Utils.git
+
+sudo mkdir presto-bk
+
+mysql -u root -proot < dbinit.sql
