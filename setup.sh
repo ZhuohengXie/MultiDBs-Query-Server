9f01ff4313547f6e02bef15d12d26c029fb4812a
ndsorrowchi <ndsorrowchi@gmail.com>
2015-03-23 21:10:05 -0400

    add missing command

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #install oracle java 8
+sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
