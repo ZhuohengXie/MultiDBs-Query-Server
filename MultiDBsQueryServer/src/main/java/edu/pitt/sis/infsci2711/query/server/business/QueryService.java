c8dac7695f493d88421eb10e0c232a05006a5dc7
ndsorrowchi <ndsorrowchi@gmail.com>
2015-04-16 22:13:56 -0400

    let ghost come

--- MultiDBsQueryServer/src/main/java/edu/pitt/sis/infsci2711/query/server/business/QueryService.java
+++ MultiDBsQueryServer/src/main/java/edu/pitt/sis/infsci2711/query/server/business/QueryService.java
@@ -159,7 +159,8 @@ public class QueryService {
 			
 				executeCreateTable(saveInfo, sql, strm, fileurl);
 				
-				boolean regSuccess = true;//registerInMetastore(saveInfo);
+				//boolean regSuccess = true;
+				boolean regSuccess = registerInMetastore(saveInfo);
 				
 				deleteCatalogIfExists(fileurl);
 
